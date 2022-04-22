a25cb5c7f3cd68903a67b3ee11f7ba0086dddb28
Ada <adabarries@gmail.com>
2022-04-22 12:14:17 -0700

    messing with debugging

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, weather):
@@ -11,3 +11,15 @@ planet_list = [Planet(1, "Mercury", "a rocky planet", "hot/dry"),
 Planet(2, "Venus", "a yellow planet", "hot/stormy"), Planet(3, "Earth", "a blue planet", "habitable")]
 
 planets_bp = Blueprint('planets', __name__, url_prefix="/planets")
+
+@planets_bp.route('', methods = ['GET'])
+def return_planet_list():
+    planet_response = []
+    for planet in planet_list:
+        planet_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "weather": planet.weather
+        })
+    return jsonify(planet_response) 
