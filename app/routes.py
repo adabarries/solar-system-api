617f1a18a3d7d6a36eb71b5b303e5982795e200e
Nikki Torab <nikkitorab@gmail.com>
2022-04-22 11:25:52 -0700

    routes

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,13 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, weather):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.weather = weather
+
+planet_list = [Planet(1, "Mercury", "a rocky planet", "hot/dry"), 
+Planet(2, "Venus", "a yellow planet", "hot/stormy"), Planet(3, "Earth", "a blue planet", "habitable")]
+
+planets_bp = Blueprint('planets', __name__, url_prefix="/planets")
